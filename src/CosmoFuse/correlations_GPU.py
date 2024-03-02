772204fcb7133b90a1ea600ce1a307a1c22413b6
David Gebauer <git@gebauer.ai>
2024-03-02 15:06:47 +0100

    Added low vram version of Correlation_GPU

--- src/CosmoFuse/correlations_GPU.py
+++ src/CosmoFuse/correlations_GPU.py
@@ -137,3 +137,90 @@ class Correlation_GPU(Correlation):
         
         return M_ap, xip.get(), xim.get()
         
+        
+        
+class Correlation_GPU_lowmem(Correlation):
+    
+    def __init__(self, nside, phi_center, theta_center, nbins=10, theta_min=10, theta_max=170, patch_size=90, theta_Q=90, mask=None, fastmath=True, device=0):
+        self.device = device
+        super().__init__(nside, phi_center, theta_center, nbins, theta_min, theta_max, patch_size, theta_Q, mask, fastmath)
+    
+    
+    def prepare_gpu(self):
+        size = 0
+        ninds = []
+        for i in range(self.n_patches):
+            patchsize = np.sum(self.bins[i])
+            size += patchsize
+            ninds.append(patchsize)
+
+        first_patch_ind = np.append(0, np.cumsum(ninds))    
+        temp_inds = np.zeros((2,size), dtype=np.uint32)
+        temp_exp2phi = np.zeros((2,size), dtype=np.complex64)
+        temp_bins = np.zeros((self.n_patches*self.nbins), dtype=np.uint32)
+        temp_bins_tot = np.zeros((self.n_patches * self.nbins), dtype=np.uint32)
+
+        for i in range(self.n_patches):
+            temp_inds[:,first_patch_ind[i]:first_patch_ind[i+1]] = self.pair_inds[i]
+            temp_exp2phi[:,first_patch_ind[i]:first_patch_ind[i+1]] = self.pair_exp2phi[i]
+            temp_bins[i*self.nbins:(i+1)*self.nbins] = self.bins[i]
+            temp_bins_tot[i*self.nbins:(i+1)*self.nbins] = first_patch_ind[i] + self.bins[i].cumsum()
+        temp_bins_tot = np.append([0], temp_bins_tot)
+        
+        self.inds_gpu = cp.asarray(temp_inds)
+        self.exp2phi_gpu = cp.asarray(temp_exp2phi)
+        self.bins_gpu = cp.asarray(temp_bins)
+        self.tot_bins_gpu = cp.asarray(temp_bins_tot)
+        self.ntotpairs = size
+    
+    
+    def xipm(self, g11, g21, g12, g22, w1, w2, sumofweights):
+        
+        g2 = w1[self.inds_gpu[0]]*((g11[self.inds_gpu[0]]) + 1j* g21[self.inds_gpu[0]]) * self.exp2phi_gpu[0]
+        g1 = w2[self.inds_gpu[1]]*((g12[self.inds_gpu[1]]) + 1j* g22[self.inds_gpu[1]]) * self.exp2phi_gpu[1]
+
+        xip, xim = cp.zeros((self.n_patches*self.nbins)), cp.zeros((self.n_patches*self.nbins))
+        
+        for tot_ind in range(self.n_patches*self.nbins):
+            xip[tot_ind] = cp.sum(g1[self.tot_bins_gpu[tot_ind]:self.tot_bins_gpu[tot_ind+1]] * cp.conjugate(g2[self.tot_bins_gpu[tot_ind]:self.tot_bins_gpu[tot_ind+1]]))/(sumofweights[tot_ind])
+            xim[tot_ind] = cp.sum(g1[self.tot_bins_gpu[tot_ind]:self.tot_bins_gpu[tot_ind+1]] * g2[self.tot_bins_gpu[tot_ind]:self.tot_bins_gpu[tot_ind+1]])/(sumofweights[tot_ind])
+        
+        return xip.real, xim.real
+    
+    
+    def get_full_tomo(self, shear_maps, w, sumofweights, flip_g1=False, flip_g2=False):
+        
+        nzbins = shear_maps.shape[0]
+        nzbin_combs = int(binom(nzbins+1, 2))
+        shear_maps_gpu = cp.asarray(shear_maps.astype(np.float(16)))
+        w_gpu = cp.asarray(w.astype(np.float16))
+        sumofweights_gpu = cp.asarray(sumofweights.astype(np.float16))
+        
+        g1, g2 = 1, 1
+        if flip_g1:
+            g1 = -1
+        if flip_g2:
+            g2 = -1
+        
+        M_ap = np.zeros([nzbins, self.n_patches])
+        xim1 = cp.zeros([nzbin_combs, self.n_patches, self.nbins])
+        xim2 = cp.zeros([nzbin_combs, self.n_patches, self.nbins])
+        xip1 = cp.zeros([nzbin_combs, self.n_patches, self.nbins])
+        xip2 = cp.zeros([nzbin_combs, self.n_patches, self.nbins])
+        
+        k=0
+        for i in range(nzbins):
+            M_ap[i] = self.get_M_a(g1*shear_maps[i,0], g2*shear_maps[i,1], w[i])
+            for j in range(i, nzbins):
+                if i == j:
+                    xip1[k], xim1[k] = self.xipm(g1*shear_maps_gpu[i,0], g2*shear_maps_gpu[i,1], g1*shear_maps_gpu[j,0], g2*shear_maps_gpu[j,1], w_gpu[i], w_gpu[j], sumofweights_gpu[0,k])
+                    xip2[k], xim2[k] = xip1[k], xim1[k]
+                else:
+                    xip1[k], xim1[k] = self.xipm(g1*shear_maps_gpu[i,0], g2*shear_maps_gpu[i,1], g1*shear_maps_gpu[j,0], g2*shear_maps_gpu[j,1], w_gpu[i], w_gpu[j], sumofweights_gpu[0,k])
+                    xip2[k], xim2[k] = self.xipm(g1*shear_maps_gpu[j,0], g2*shear_maps_gpu[j,1], g1*shear_maps_gpu[i,0], g2*shear_maps_gpu[i,1], w_gpu[j], w_gpu[i], sumofweights_gpu[1,k])
+                k += 1
+                
+        xip = (xip1 + xip2)/2
+        xim = (xim1 + xim2)/2
+        
+        return M_ap, xip.get(), xim.get()
